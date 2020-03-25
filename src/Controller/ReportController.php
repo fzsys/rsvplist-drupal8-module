0635cd028c36b07ff0a5486cf64a3639de470647
Igor Uhan <uhan.igor@gmail.com>
2020-03-25 18:18:43 +0200

    added rsvp report

---
+++ src/Controller/ReportController.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\rsvplist\Controller\ReportController
+ */
+
+namespace Drupal\rsvplist\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Database;
+
+/**
+ * Class ReportController
+ * @package Drupal\rsvplist\Controller
+ */
+class ReportController extends ControllerBase
+{
+  /**
+   * Get all RSVPs for all nodes.
+   *
+   * @return array
+   */
+  protected function load() {
+    $select = Database::getConnection()->select('rsvplist', 'r');
+    $select->join('users_field_data', 'u', 'r.uid=u.uid');
+    $select->join('node_field_data', 'n', 'r.nid=n.nid');
+    $select->addField('u', 'name', 'username');
+    $select->addField('n', 'title');
+    $select->addField('r', 'mail');
+    $entries = $select->execute()->fetchAll(\PDO::FETCH_ASSOC);
+    return $entries;
+  }
+
+  /**
+   * Creates the report page
+   *
+   * @return array
+   *   Render array for report output
+   */
+  public function report()
+  {
+    $content = [];
+    $content['message'] = [
+      '#markup' => $this->t('List of all events RSVPs'),
+
+    ];
+    $headers = [
+      t('Name'),
+      t('Event'),
+      t('Email'),
+    ];
+    $rows = [];
+    foreach ($entries = $this->load() as $entry) {
+      $rows[] = array_map('Drupal\Component\Utility\SafeMarkup::checkPlain', $entry);
+    }
+    $content['table'] = [
+      '#type' => 'table',
+      '#header' => $headers,
+      '#rows' => $rows,
+      '#empty' => t('No entries available'),
+    ];
+    $content['#cache']['max-age'] = 0;
+    return $content;
+  }
+}
